3eb86b18ea2357ef94784b28a36c1e0c458dcd5f
rainchensh <81663884+rainchensh@users.noreply.github.com>
2023-02-21 09:59:04 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -12,11 +12,12 @@
 
 # Modify default IP
 # sed -i 's/192.168.1.5/192.168.199.1/g' package/base-files/files/bin/config_generate
-#update golang 19.x (Fix build for openwrt-21.02/22.03 branches)
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+
 # Add luci-app-ssr-plus
 pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
 git clone --depth=1 https://github.com/sbwml/luci-app-alist
 popd
+#update golang 19.x (Fix build for openwrt-21.02/22.03 branches)
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
