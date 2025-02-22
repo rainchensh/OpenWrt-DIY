8a226b89f1fadf74b6de82595330bc3393ae26d8
rainchensh <81663884+rainchensh@users.noreply.github.com>
2025-02-22 22:54:23 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -12,6 +12,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # svn export https://github.com/openwrt/packages/branches/openwrt-21.02/libs/libaio packages/libs/libaio
 # echo 'src-git openvswitch git://github.com/pichuang/openvwrt.git' >> feeds.conf
 # echo 'src-git packages https://git.openwrt.org/feed/packages.git' >> feeds.conf.default
-echo 'src-git luci https://git.openwrt.org/project/luci.git' >> feeds.conf.default
-echo 'src-git routing https://git.openwrt.org/feed/routing.git' >> feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >> feeds.conf.default
+# echo 'src-git luci https://git.openwrt.org/project/luci.git' >> feeds.conf.default
+# echo 'src-git routing https://git.openwrt.org/feed/routing.git' >> feeds.conf.default
+# echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >> feeds.conf.default
