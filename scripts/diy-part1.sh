214acb4e2df53ae5ea77e6ae053a77c213988825
rainchensh <81663884+rainchensh@users.noreply.github.com>
2025-02-22 22:37:14 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -10,7 +10,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
 # mkdir package/libs/libaio
 # svn export https://github.com/openwrt/packages/branches/openwrt-21.02/libs/libaio packages/libs/libaio
-src-git packages https://git.openwrt.org/feed/packages.git
-src-git luci https://git.openwrt.org/project/luci.git
-src-git routing https://git.openwrt.org/feed/routing.git
-src-git telephony https://git.openwrt.org/feed/telephony.git
+# echo 'src-git openvswitch git://github.com/pichuang/openvwrt.git' >> feeds.conf
+echo 'src-git packages https://git.openwrt.org/feed/packages.git' >> feeds.conf.default
+echo 'src-git luci https://git.openwrt.org/project/luci.git' >> feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git' >> feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >> feeds.conf.default
