fc6de041c9a0f445b4c13e50bf077d505bb4d228
rainchensh <81663884+rainchensh@users.noreply.github.com>
2025-03-12 17:16:10 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -10,21 +10,21 @@ sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_gener
 # svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 #git clone https://github.com/sbwml/luci-app-alist package/alist
 #add sirpdboy's luci-app-netspeedtest
-git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+# git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 git clone https://github.com/msylgj/luci-app-tencentddns.git package/luci-app-tencentddns
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
 
 # Add luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # Add luci-app-vssr <M>
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
 
 # Add OpenClash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+# git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
 # Add luci-app-dockerman
 #rm -rf ../lean/luci-app-docker
